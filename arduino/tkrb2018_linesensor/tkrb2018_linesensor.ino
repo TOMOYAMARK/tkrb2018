101dbe86225fa5fb43e5cd0e1a818518a57dbbb2
toffeeeeeee <toffeeeeeee@noreply.github.com>
2018-10-08 21:39:35 +0900

    Some changes

--- arduino/tkrb2018_linesensor/tkrb2018_linesensor.ino
+++ arduino/tkrb2018_linesensor/tkrb2018_linesensor.ino
@@ -1,7 +1,7 @@
-//#define USE_USBCON
 #include <ros.h>
 #include <std_msgs/Int8.h>
 #include <std_msgs/Int8MultiArray.h>
+#include <std_msgs/MultiArrayDimension.h>
 
 //フォトダイオードの位置とピンを関連付ける
 //pinAssign[0]が左端
@@ -9,21 +9,29 @@
 static const int pinAssign[S_NUMBER] = {0,1,2,3,4,5,6,7};
 
 ros::NodeHandle  nh;
-std_msgs::Int8MultiArray sensor_value;
-ros::Publisher linesensor("linesensor", &sensor_value);
+std_msgs::Int8MultiArray msg;
+ros::Publisher linsensor_publisher("linsensor_publisher", &msg);
 
 void setup()
 {
   nh.initNode();
-  nh.advertise(linesensor);
+  msg.layout.dim=(std_msgs::MultiArrayDimension *)
+  malloc(sizeof(std_msgs::MultiArrayDimension)*2);
+  msg.layout.dim[0].size=sizeof(int[8]);
+  msg.layout.dim[0].stride=8;
+  msg.layout.dim[0].label="line_sensor";
+  msg.layout.data_offset = 0;
+  msg.layout.dim_length=1;
+  msg.data_length=sizeof(int[8]);
+  nh.advertise(linsensor_publisher);
 }
 
 void loop()
 {
-  linesensor.publish( &sensor_value );
   for(int i=0; i<S_NUMBER; i++) {
-    sensor_value.data[i] = analogRead(pinAssign[i]);
+    msg.data[i] = analogRead(pinAssign[i]);
   }
+  linsensor_publisher.publish(&msg);
   nh.spinOnce();
   delay(1000);
 }
