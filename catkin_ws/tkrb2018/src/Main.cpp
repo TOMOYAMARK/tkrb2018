62f27deccdc12d97241c888a01b4850f97f9fffd
toffeeeeeee <toffeeeeeee@noreply.github.com>
2018-10-11 20:21:39 +0900

    Fixed typo

--- catkin_ws/tkrb2018/src/Main.cpp
+++ catkin_ws/tkrb2018/src/Main.cpp
@@ -374,10 +374,11 @@ void setTarget(char t, double par){
     }
   case 'a':
     {
-      //射出用エアシリンダの伸縮制御　trueでのばす、falseで縮める
-      std_msgs::Bool willShoot;
-      willShoot.data = true;
-      airSylinderRequest.publish(willShoot); //存在しない関数
+      //射出用エアシリンダの伸縮制御　非負整数で射出から収縮まで
+      //
+      std_msgs::Int16 waitSeconds; //暫定でInt16; pythonで設定したほうが楽な値なので
+      willExtend.data = true;
+      airCylinderRequest.publish(willExtend); //存在しない関数
       state = WORKING; //撃ちながら動くと安定しない可能性
       break;
     }
