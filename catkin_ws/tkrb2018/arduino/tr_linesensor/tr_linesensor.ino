d8d44ed2b1353f41ae14c3401e17788c924f5e4e
toffeeeeeee <toffeeeeeee@noreply.github.com>
2018-10-13 06:43:11 +0900

    Add arduino side program

--- catkin_ws/tkrb2018/arduino/tr_linesensor/tr_linesensor.ino
+++ catkin_ws/tkrb2018/arduino/tr_linesensor/tr_linesensor.ino
@@ -1,5 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <ros.h>
-#include <std_msgs/Int16.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/Int32MultiArray.h>
+
+#include "std_msgs/MultiArrayLayout.h"
+#include "std_msgs/MultiArrayDimension.h"
 
 //フォトダイオードの位置とピンを関連付ける
 //pinAssign[0]が左端
@@ -8,29 +15,23 @@ static const int pinAssign[S_NUMBER] = {0,1,2,3,4,5,6,7};
 
 ros::NodeHandle  nh;
 //つくろぼのgithub見て修正
-std_msgs::Int16 msgs[8] = {};
-ros::Publisher pub0("ls0", &msgs[0]);
-ros::Publisher pub1("ls1", &msgs[1]);
-ros::Publisher pub2("ls2", &msgs[2]);
-ros::Publisher pub3("ls3", &msgs[3]);
-ros::Publisher pub4("ls4", &msgs[4]);
-ros::Publisher pub5("ls5", &msgs[5]);
-ros::Publisher pub6("ls6", &msgs[6]);
-ros::Publisher pub7("ls7", &msgs[7]);
-ros::Publisher pub[8] = {pub0, pub1, pub2, pub3, pub4, pub5, pub6, pub7};
+std_msgs::Int32MultiArray msgs;
+ros::Publisher pub("ls", &msgs);
+long ls[8] = {};
 
 void setup()
 {
   nh.initNode();
-  for(int i=0; i<S_NUMBER; i++)
-    nh.advertise(pub[i]);
+  nh.advertise(pub);
 }
 
 void loop()
 {
-  for(int i=0; i<S_NUMBER; i++) {
-    msgs[i].data = analogRead(pinAssign[i]);
-    pub[i].publish(&msgs[i]);
+  msgs.data_length=8;
+  for(int i=0;i<8;i++){
+    ls[i] = analogRead(pinAssign[i]);
   }
+  msgs.data = ls;
+  pub.publish(&msgs);
   nh.spinOnce();
 }
