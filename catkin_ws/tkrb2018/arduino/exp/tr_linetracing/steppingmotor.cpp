af1a26eef36aeefb20e28e5e9caab5959e43fe3f
toffeeeeeee <38903670+toffeeeeeee@users.noreply.github.com>
2018-10-19 23:48:19 +0900

    Removed some directories which is not used now

--- catkin_ws/tkrb2018/arduino/exp/tr_linetracing/steppingmotor.cpp
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-#include "steppingmotor.h"
-
-SteppingMotor::SteppingMotor(steppingMotorPinAssign pin, float stepAngle) {
-    this->pin = pin;
-    this->stepAngle = stepAngle;
-    pinMode(pin.cw, OUTPUT);
-    pinMode(pin.ccw, OUTPUT);
-}
